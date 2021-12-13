5dbea67ad4c8db84068a4bd0821e3dee782499a8
Hrybeniuk Kateryna <dp040497see@gmail.com>
2021-12-13 17:01:07 +0200

    added function

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-function typeNum () {
+function typeNum() {
     let num;
-    while (num !== "exit") {
-        num = +prompt('Enter your number');
-        if (num >0) {
-           alert('Your number positive');
-        } else if (num < 0) {
-            alert('your number negative'); 
-        } else if (num == 0) {
+    do {
+        num = prompt('Enter your number');
+        if (Number(num) > 0) {
+            alert('Your number positive');
+        } else if (Number(num) < 0) {
+            alert('your number negative');
+        } else if (Number(num) == 0) {
             alert('Its null');
-        } else {
+        } else if (num == 'exit') { break; }
+        else {
             alert('Enter your number!');
+
         }
-        }
-    }
-   
-    typeNum();
+
+    } while (num !== 'exit')
+};
+
+typeNum();
+
